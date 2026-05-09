53fb4e2ff8031a1863c65efdfd064cdacefef948
rahul-kalwar <rahulkumarkalwar40@gmail.com>
2026-05-09 16:03:14 +0300

    Recreated api.js and authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,10 @@
 import API from "./api";
 
-// REGISTER
 export const registerUser = async (data) => {
   const res = await API.post("/auth/register", data);
   return res.data;
 };
 
-// LOGIN
 export const loginUser = async (data) => {
   const res = await API.post("/auth/login", data);
   return res.data;
